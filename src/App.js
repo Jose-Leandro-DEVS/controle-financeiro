f91d9b89471ade1b4466db76c5a94e61b39fb20a
José Leandro <jmendes@marinformatica.com.br>
2022-09-22 15:25:25 -0300

    Controle-financeiro - 22/09

--- src/App.js
+++ src/App.js
@@ -5,11 +5,37 @@ import Resume from './components/Resume';
 import Global from './styles/global';
 const App = () => {
     const data = localStorage.getItem("transactions");
-    
+    const [transactionsList, setTransactionsList] = useState(
+        data ? JSON.parse(data) : []
+    );
+
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const amountExpense = transactionsList
+            .filter((item) => item.expense)
+            .map((transaction) => Number(transaction.amount));
+
+        const amountIncome = transactionsList
+            .filter((item) => item.expense)
+            .map((transaction) => Number(transaction.amount));
+
+        const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+        const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+        const total = Math.abs(income - expense).toFixed(2);
+
+        setIncome(`R$ ${income}`);
+        setExpense(`R$ ${expense}`);
+        setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
+
+     }, [transactionsList]);
     return(
         <>
             <Header />
-            <Resume />
+            <Resume income={income} expense={expense} total={total} />
             <Global />
             <Form />
         </>
